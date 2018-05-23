455d7b105f8aa97f5937a947dad4d9a6228a8d64
quang <thangnguyen2000x99nd@gmail.com>
2018-05-23 17:38:25 +0700

    app

--- static/hello.js
+++ static/hello.js
@@ -1,10 +1,3 @@
-var me = {};
-me.avatar = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
-
-var you = {};
-you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
-
-
 $( document ).ready(function() {
 
 
@@ -25,27 +18,16 @@ function insertChat(who, text, time){
     }
     var control = "";
     var date = formatAMPM(new Date());
-
-    if (who == "me"){
-        control = '<li style="width:100%">' +
+    control = '<li style="width:100%">' +
                         '<div class="msj macro">' +
-                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ who.avatar +'" /></div>' +
                             '<div class="text text-l">' +
                                 '<p>'+ text +'</p>' +
                                 '<p><small>'+date+'</small></p>' +
                             '</div>' +
                         '</div>' +
                     '</li>';
-    }else{
-        control = '<li style="width:100%;">' +
-                        '<div class="msj-rta macro">' +
-                            '<div class="text text-r">' +
-                                '<p>'+text+'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +
-                  '</li>';
-    }
+
     setTimeout(
         function(){
             $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
